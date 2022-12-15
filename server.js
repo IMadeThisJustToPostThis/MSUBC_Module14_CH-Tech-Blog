33ec41a9985a9b426ab4d8eb18d89a08d60345aa
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-12-15 15:45:50 -0500

    setup sequelize session

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connection = require('./config/connection.js'); // connection to database,
 const routes = require('./controllers/'); // controllers
 const express = require('express'); // express middleware
 const session = require('express-session'); // session for authentication
-const store = require('connect-session-sequelize')(session.Store); // session store
+const SequelizeStore = require('connect-session-sequelize')(session.Store); // session store
 const handlebars = require('express-handlebars'); // handlebars for views
 const path = require('path'); // path for environment paths
 
@@ -18,7 +18,18 @@ app.set('view engine', 'handlebars');
 
 //setup session
 const sess = {
-
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 3600000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict'
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: connection
+    })
 };
 
 //setup application
@@ -26,9 +37,9 @@ app.use(express.json()); // parsing json
 app.use(express.urlencoded({ extended: false })); // parsing urls
 app.use(express.static(path.join(__dirname, 'public'))); // serve static files
 //app.use(routes); // connect routes to app
-//app.use(session(sess)); // express session
+app.use(session(sess)); // express session
 
-// 
+// sync sequelize models to the database, then turn on the server
 connection.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`Listening at: http://localhost:${PORT}`);
